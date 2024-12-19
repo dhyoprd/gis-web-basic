c7e793587fd094b08504acab1a3f10bbb6f6d9d8
Dhyo Pradnyadiva <dhyoprd@gmail.com>
2024-12-20 02:07:56 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,8 @@ use App\Http\Controllers\MarkerController;
 use App\Http\Controllers\PolygonController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-Route::get('/', function () {
-    return view('map');
-});
-=======
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
-
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-Route::get('/tugas-handson-1', [HandsOnController::class, 'tugas1'])->name('tugas-handson-1');
-Route::get('/tugas-handson-2', [HandsOnController::class, 'tugas2'])->name('tugas-handson-2');
-
-=======
 
 /*
 |--------------------------------------------------------------------------
@@ -56,7 +35,3 @@ Route::post('/api/polygons', [PolygonController::class, 'store']);
 Route::delete('/api/polygons/{id}', [PolygonController::class, 'destroy']);
 
 Route::get('/home', [DashboardController::class, 'index'])->name('home');
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
